e16ed9a9abd2209b799194f7f8b7671e7fa2a012
Lamide <oluborodeemma1@gmail.com>
2026-05-14 17:00:53 +0100

    made changes

--- Controller/categorySection.js
+++ Controller/categorySection.js
@@ -1,1 +1,119 @@
-const catergory = document.get
+const categoryBox = document.getElementById("categoryBox");
+
+const categories = [
+  {
+    id: 1,
+    icon: "cake.svg",
+    title: "Cakes & Pastries",
+    subCategories: ["cake1", "cake2", "cake3", "cake4"],
+  },
+  {
+    id: 2,
+    icon: "",
+    title: "Electronics",
+    subCategories: ['laptops', 'lamps', 'Gadgets', 'electric kettles'],
+  },
+  {
+    id: 3,
+    icon: "",
+    title: "Groceries",
+    subCategories: ['laptops', 'lamps', 'Gadgets', 'electric kettles'],
+  },
+  {
+    id: 4,
+    icon: "",
+    title: "Phones",
+    subCategories: ['laptops', 'lamps', 'Gadgets', 'electric kettles'],
+  },
+  {
+    id: 5,
+    icon: "",
+    title: "Laptops",
+    subCategories: ['laptops', 'lamps', 'Gadgets', 'electric kettles'],
+  },
+  {
+    id: 6,
+    icon: "",
+    title: "Wine & Spirits",
+    subCategories: ['laptops', 'lamps', 'Gadgets', 'electric kettles'],
+  },
+  {
+    id: 7,
+    icon: "",
+    title: "Home Furnishing",
+    subCategories: ['laptops', 'lamps', 'Gadgets', 'electric kettles'],
+  },
+  {
+    id: 8,
+    icon: "",
+    title: "Stationery",
+    subCategories: ['laptops', 'lamps', 'Gadgets', 'electric kettles'],
+  },
+  {
+    id: 9,
+    icon: "",
+    title: "Toys & Games",
+    subCategories: ['laptops', 'lamps', 'Gadgets', 'electric kettles'],
+  },
+  {
+    id: 10,
+    icon: "",
+    title: "Lifestyle",
+    subCategories: ['laptops', 'lamps', 'Gadgets', 'electric kettles'],
+  },
+  {
+    id: 11,
+    icon: "",
+    title: "Butchery",
+    subCategories: ['laptops', 'lamps', 'Gadgets', 'electric kettles'],
+  },
+  {
+    id: 12,
+    icon: "",
+    title: "Fresh Fruits & Vegetables",
+    subCategories: ['laptops', 'lamps', 'Gadgets', 'electric kettles'],
+  },
+];
+
+const buildCategoryContent = () => {
+  categories.map((item, index) => {
+    return buildEachCategory(item.title, item.icon, item.subCategories);
+  });
+};
+
+const buildEachCategory = (label, icon, subCategories) => {
+  const eachCategory = document.createElement("div");
+  eachCategory.className = "each-category";
+  eachCategory.innerHTML = `
+    <div style="margin-bottom: 14px; padding-bottom:10px;.;l border-bottom: 1px solid #D9D9D9; width:100%; display:flex; flex-direction:row; justify-content:space-between; align-items:center">
+      <div style="display:flex; flex-direction:row; gap:15px; align-items:center; font-size:14px; color:#090B0E;"> 
+        <img
+          src="./Assests/icons/${icon}"
+          alt="cake-icon"          
+          width="24px"
+        />
+        <p>${label}</p>
+      </div>
+      
+      <img 
+        src="./Assests/icons/ChevronRight.svg"
+        alt="chevron-right"
+        width="20px"
+      />
+    </div>    
+      `;
+    subCategories.map((item, index)=>{
+        return buildSubCategory(item, eachCategory)
+    })
+  categoryBox.appendChild(eachCategory);
+};
+
+const buildSubCategory = (item, eachCategory) =>{
+    const subCategory = document.createElement('div');
+    subCategory.className = 'subcategory'
+    subCategory.innerHTML = `<p> ${item} </p>`
+
+    eachCategory.appendChild(subCategory)
+}
+
+buildCategoryContent();
---
+++ Controller/learning.js
@@ -0,0 +1,227 @@
+// let result = "";
+// for (let i = 1; i < 8; i++) {
+//     result = result + "#";
+//     console.log(result);
+// }
+
+// for (let i = 1; i < 101; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// console.log(7%2)
+// console.log(30%3)
+
+// console.log("first man\n Second man")
+
+
+// let chessBoard = "";
+// for (let i = 0; i < 8; i++) {
+//     let line = "";
+//     for (let j = 0; j < 8; j++) {
+//         if ((i + j) % 2 === 0) {
+//             line = line + " ";
+//         } else {
+//             line = line + "#";
+//         }
+//     }
+//     chessBoard = chessBoard + line + "\n";
+// }
+// console.log(chessBoard)
+
+// switch ("") {
+//     case "": 
+//     console.log(first)
+//     break
+// }
+
+
+// const calc = birthYear => 2037 - birthYear;
+
+// const roundTo = (n, step) => {
+
+// }
+
+// function greet (who) {
+//     console.log("Hello " + who);
+// }
+// greet("Harry");
+// console.log("Bye")
+
+/*
+function chicken() {
+    return egg();
+}
+
+function egg() {
+    return chicken();
+}
+
+console.log(chicken() + "came first")
+console.log(egg() + "came first")
+*/
+
+// function square (x) {
+//     return x**2; 
+// }
+// // console.log(square(4, true, "hedgehog"))
+// console.log(square(true, false, "hedgehog"))
+// // console.log(4,6)
+
+/*
+function add(a, b) {
+    return a + b
+}
+console.log(add(5));
+*/
+/*
+function minus(a, b = 0) {
+    if (b === undefined) 
+        return -a;
+    else 
+        return a - b
+}
+console.log(minus(10));
+// console.log(minus(10, 5));
+// console.log(minus(5, 10));
+*/
+
+/*
+function roundTo(n, step = 1) {
+    let remainder = n % step;
+    return n - remainder + (remainder < step / 2 ? 0 : step);
+};
+
+// console.log(roundTo(4.5))
+console.log(roundTo())
+// console.log(roundTo(1)) */
+
+/* closure function */
+// function wrapValue(n) {
+//     let local = n;
+//     return () => local;
+// }
+
+// let wrap1 = wrapValue(1);
+// let wrap2 = wrapValue(2);
+// console.log(wrap1());
+// console.log(wrap2());
+
+/*
+function multiplier(factor) {
+    return number => number * factor;
+}
+
+let twice = multiplier(2);
+console.log(twice(0)); */
+
+
+// function power (base, exponent) {
+//     if (exponent == 0) {
+//         return 1;
+//     } else {
+//         return base * power(base, exponent - 1)
+//     }
+// }
+
+// // console.log(power(2, 3)); //8
+// console.log(power(3, 2)); //9
+
+
+// let listOfNumbers = [2, 3, 5, 7, 11]
+// console.log(listOfNumbers[2 - 1]);
+
+// let person = {
+//     name: "lamide",
+//     age: 20
+// };
+// console.log(person.name);
+// console.log(person["name"]);
+
+// let day1 = {
+//     squirrel: false,
+//     events: ["work", "tree", "pizza", 67, "67", "false", "true"]
+// }
+
+// console.log(day1.squirrel);
+// console.log(day1.wolf);
+// console.log(day1["squirrel"]);
+// console.log(day1.events[0]);
+
+
+// let person = {
+//     name: "lamide"
+// }
+
+// console.log(person["name"])
+// console.log(person.name)
+
+// let test = n => ({prop: n})
+// console.log(test(5))
+
+/*
+let animal = "cat"
+animal = "rat"
+
+console.log(animal)
+
+let person = {
+    name: "lamide"
+}
+// person.name = "John"
+person["name"] = "John"
+
+console.log(person.name)
+console.log(person["name"])
+
+let object1 = {
+    value: 10
+}
+
+object1.value = "15"
+console.log(object1.value)
+console.log(object2.value)
+let object2 = object1;
+// console.log(object1 == object2)
+// console.log(object1 === object2)
+
+let object3 = {
+    value: 10
+}
+
+console.log(object1 == object3) 
+
+const score = {
+    visitors: 0,
+    home: 0
+};
+
+goal == score;
+score.visitors = 1;
+console.log(score.visitors)
+console.log(score.home) 
+
+let user = {
+    name: "sam",
+    address = {
+        city: "lagos",
+        state: "lagos"
+    }
+}
+// console.log(user.address.city);
+console.log(user[address[city]]);
+
+for (let i = 0; i < 5; i++) {
+    console.log(i)
+} */
+
+for (let number = 0; number <= 12; number = number + 2) {
+    console.log(number)
+}
